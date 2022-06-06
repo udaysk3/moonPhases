951763e2561660f07f1c628a268b57897d6156aa
UdaySantoshKumar <75845600+udaysk3@users.noreply.github.com>
2022-06-06 22:51:03 +0530

    Update settings.py

--- moonPhases/settings.py
+++ moonPhases/settings.py
@@ -131,11 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,''),
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'home/static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
