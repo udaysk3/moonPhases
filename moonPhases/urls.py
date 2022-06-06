694cf71e01e756a3722e06e55175e6ba7501b68e
UdaySantoshKumar <75845600+udaysk3@users.noreply.github.com>
2022-06-06 23:58:53 +0530

    Update urls.py

--- moonPhases/urls.py
+++ moonPhases/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
