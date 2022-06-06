c3c1f119330e75ad499af140ed5e93652028d0a5
UdaySantoshKumar <75845600+udaysk3@users.noreply.github.com>
2022-06-07 00:16:41 +0530

    Add files via upload

--- moonPhases/urls.py
+++ moonPhases/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.views.static import serve
 from django.urls import re_path as url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,4 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
